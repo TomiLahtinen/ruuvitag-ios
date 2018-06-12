eb75988d3a253895643eee87cf346ba2314b2407
Tomi Lahtinen <tomi.lahtinen@solita.fi>
2018-06-12 14:24:47 +0300

    Initial Commit

---
+++ ruuvitag-ios/ruuvitag_ios.h
@@ -0,0 +1,19 @@
+//
+//  ruuvitag_ios.h
+//  ruuvitag-ios
+//
+//  Created by Tomi Lahtinen on 12/06/2018.
+//  Copyright © 2018 Tomi Lahtinen. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for ruuvitag_ios.
+FOUNDATION_EXPORT double ruuvitag_iosVersionNumber;
+
+//! Project version string for ruuvitag_ios.
+FOUNDATION_EXPORT const unsigned char ruuvitag_iosVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ruuvitag_ios/PublicHeader.h>
+
+
